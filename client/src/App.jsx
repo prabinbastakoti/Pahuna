3a2dbb4144e7d3ed9b7b62e9b515fbda412fed04
Prabin Bastakoti <101822704+PrabinBastakoti@users.noreply.github.com>
2023-09-25 19:00:29 +0545

    logout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,18 +4,14 @@ import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import Layout from './Layout';
 import SignupPage from './pages/SignupPage';
-import { useContext } from 'react';
-import { AuthContext } from './context/AuthContext';
 
 function App() {
-  const { user } = useContext(AuthContext);
-
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<HomePage />} />
-        <Route path="/login" element={!user && <LoginPage />} />
-        <Route path="/signup" element={!user && <SignupPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
       </Route>
     </Routes>
   );

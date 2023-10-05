72294e9106bbba5088466778917c76d5d41b06d5
Prabin Bastakoti <101822704+PrabinBastakoti@users.noreply.github.com>
2023-10-05 17:02:15 +0545

    new proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,9 +5,15 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
+    // proxy: {
+    //   '/api': {
+    //     target: 'http://localhost:3001',
+    //     changeOrigin: true,
+    //   },
+    // },
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'https://airbnb-api-9urn.onrender.com',
         changeOrigin: true,
       },
     },

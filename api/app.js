014077f676d6dcdf16583bf088cd2964a9c39846
Prabin Bastakoti <Prabinbastakoti1@gmail.com>
2024-01-19 13:40:57 +0545

    new solution

--- api/app.js
+++ api/app.js
@@ -1,7 +1,7 @@
 require('express-async-errors');
 const express = require('express');
 const app = express();
-const cors = require('cors');
+// const cors = require('cors');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const middleware = require('./utils/middleware');
@@ -24,13 +24,35 @@ mongoose
     logger.error('MongoDB Disconnected');
   });
 
-app.use(
-  cors({
-    origin: 'https://pahuna-client.onrender.com/',
-    credentials: true, //access-control-allow-credentials:true
-    optionSuccessStatus: 200,
-  })
-);
+// app.use(
+//   cors({
+//     origin: 'https://pahuna-client.onrender.com/',
+//     credentials: true, //access-control-allow-credentials:true
+//     optionSuccessStatus: 200,
+//   })
+// );
+
+app.use((req, res, next) => {
+  res.setHeader(
+    'Access-Control-Allow-Origin',
+    'https://pahuna-client.onrender.com/'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers'
+  );
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Private-Network', true);
+  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+  res.setHeader('Access-Control-Max-Age', 7200);
+
+  next();
+});
+
 app.use(cookieParser());
 app.use(express.json());
 app.use('/api/uploads', express.static(__dirname + '/uploads'));

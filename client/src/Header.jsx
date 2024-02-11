d97d1af75d90c0539eb12849206f035c972f31dd
Prabin Bastakoti <Prabinbastakoti1@gmail.com>
2024-02-11 21:57:27 +0545

    fix typo

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -1,6 +1,6 @@
 import { useState, useRef, useEffect, useContext } from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
-import Logo from './assets/Logo.svg';
+import Logo from './assets/logo.svg';
 import { AuthContext } from './context/AuthContext';
 import authService from './services/authService';
 

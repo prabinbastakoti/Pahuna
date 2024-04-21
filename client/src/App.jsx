441f12861f3d5d829ba0fb5bb203080a9f42abb5
Prabin Bastakoti <Prabinbastakoti1@gmail.com>
2024-04-21 08:24:34 +0545

    remove app.css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,4 @@
 import { Route, Routes } from 'react-router-dom';
-import './App.css';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import Layout from './Layout';
